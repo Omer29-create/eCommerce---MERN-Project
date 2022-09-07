6edc464d09bd5f84e6d80d0532cb2f1737882124
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2022-09-07 22:16:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const productRouter = require('./router/productRouter')
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
-
-
 require('./db/db')
 // app.get('/', (req, res) => {res.send('index')})
 
